30c7592609f71a34b88ce1900ca4b1ee7782654b
xu renlu <renlu.xu@xiaoqianbao.com>
2013-05-26 01:53:54 +0800

    nothing

--- js/main.js
+++ js/main.js
@@ -8,15 +8,24 @@ $(function(){
     var markGiuApp = new markgiu.AppGui();
     markGiuApp.bindChoosers('#choosefile', '#savefileas');
     ko.applyBindings(markGiuApp, $("#wrap")[0]);
-    markGiuApp.addTabNew();
+    
     //show window
     gui.Window.get().show();
     //console.log(gui.App.argv[0]);
     window.argv=gui.App.argv;
+    if(window.argv.length>0){
+        var arg_index;
+        for(arg_index in window.argv){
+            markGiuApp.openFile(window.argv[arg_index]);
+        }
+    }else{
+        markGiuApp.addTabNew();    
+    }
     gui.App.on('open', function(path) {
     	markGiuApp.openFile(path);
   		console.log('[msg]Opening: ' + path);
 	});
+    $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
     //setInterval(function(){$('pre code').each(function(i, e) {hljs.highlightBlock(e)});},1000);
 });
 function viewInHtml(){
